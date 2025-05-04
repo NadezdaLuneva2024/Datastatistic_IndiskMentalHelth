9f62eed2b6897aae5f6217afc92bc8978f67d496
NadezdaLuneva2024 <luna2004198816@gmail.com>
2025-05-04 19:19:59 +0200

    ren inlämning

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,6 @@
 
 createMenu('Depress mellan indiska studenter', [
-  { name: 'Översikt och Jämförelse mellan kön', script: 'fa.js' },
-  { name: 'Sambandsanalys mellan faktorer', script: 'jamfora_objektiva_data.js' },
-  { name: 'Samband med depression', script: 'jamfora_subjektiva_data.js' },
+  { name: 'Översikt och Jämförelse mellan kön', script: 'oversikt.js' },
+  { name: 'Sambandsanalys mellan faktorer', script: 'olikaFaktorer.js' },
+  { name: 'Samband med depression', script: 'vadPaverkarDerpession.js' },
 ]);
